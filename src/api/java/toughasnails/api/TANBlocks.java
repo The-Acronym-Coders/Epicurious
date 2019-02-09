64f66599ec95441f24e5fd7a6de6483950eff748
Lanse <simon_stalnabb@hotmail.com>
2019-02-09 20:39:16 +0100

    Added Tough As Nails API

---
+++ src/api/java/toughasnails/api/TANBlocks.java
@@ -0,0 +1,15 @@
+package toughasnails.api;
+
+import net.minecraft.block.Block;
+import net.minecraftforge.fluids.Fluid;
+
+public class TANBlocks
+{
+    public static Block campfire;
+    public static Block gas;
+    public static Block temperature_coil;
+    public static Block rain_collector;
+    
+    public static Block purified_water;
+    public static Fluid purified_water_fluid;
+}
