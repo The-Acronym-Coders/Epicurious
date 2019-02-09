186f675780d1926df3e3306ce1cd5e2ced04faa5
Lanse <simon_stalnabb@hotmail.com>
2019-02-09 22:18:07 +0100

    Actually added proper deps

--- src/main/java/com/teamacronymcoders/epicurious/Epicurious.java
+++ src/main/java/com/teamacronymcoders/epicurious/Epicurious.java
@@ -30,7 +30,13 @@ public class Epicurious extends BaseModFoundation<Epicurious> {
     public static final String VERSION = "1.12.2-1.0.0";
     public static final String MCVERSION = "1.12,";
     public static final String DEPS = "" +
-            "required-after:patchouli@[1.0-13,);";
+            "required-after:survivalism;" +
+            "after:crafttweaker;" +
+            "after:hwyla;" +
+            "after:immersiveengineering;" +
+            "after:theoneprobe;" +
+            "after:toughasnails;" +
+            "before:patchouli;";
 
     private static final String COMMON = "com.teamacronymcoders.epicurious.common.CommonProxy";
     private static final String CLIENT = "com.teamacronymcoders.epicurious.client.ClientProxy";
